ab6627bf1850f3e3d0d91a444e2c66ae48ea392d
Jenny <jhsu2@hawaii.edu>
2020-10-03 02:54:44 -1000

    dynamically reads in data

---
+++ app/src/ui/components/ProjectsCard.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Card, Header, Item, Image, Label, Button, Icon } from 'semantic-ui-react';
+
+class ProjectsCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    const renderLabels = (tool) => {
+      return (
+          <Label style={{ backgroundColor: '#00000059', color: 'white' }}>
+            {tool}
+          </Label>
+      )
+    };
+
+    return (
+        <Item.Group divided
+                    style={{
+                      backgroundColor: 'rgba(255, 255, 255, 0.10)',
+                      padding: '3rem 3rem',
+                      marginBottom: '1rem',
+                    }}
+                    className={'project'}>
+          <Item>
+            <Item.Image
+                src={this.props.project.image}/>
+            <Item.Content>
+              <Item.Header as='a'>{this.props.project.name}</Item.Header>
+              <Item.Meta>
+                    <span style={{ color: 'white' }}>
+                       {this.props.project.date}
+
+                    </span>
+              </Item.Meta>
+              <Item.Description style={{ color: 'white' }}>
+                {this.props.project.shortDesp}
+              </Item.Description>
+              <Item.Extra>
+                {this.props.project.tools.map((skill) => (
+                    // <Label circular key={skill} style={skillsStyle}>
+                    //   {skill}
+                    // </Label>
+                    renderLabels(skill)
+                ))}
+                <Button floated='right' style={{ backgroundColor: '#3c4044', color: 'white' }}>
+                  Read More
+                  <Icon name='right chevron'/>
+                </Button>
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+
+    )
+  }
+}
+
+export default ProjectsCard;
