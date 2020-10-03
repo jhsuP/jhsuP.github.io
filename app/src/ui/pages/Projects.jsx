d64a304e65411c48b514ca56584b708282c700a2
Jenny <jhsu2@hawaii.edu>
2020-10-03 02:59:44 -1000

    updated projects

--- app/src/ui/pages/Projects.jsx
+++ app/src/ui/pages/Projects.jsx
@@ -9,48 +9,12 @@ class Projects extends React.Component {
 
   render() {
 
-
     return (
         <div id={'menuBG'}>
-          <Container style={{ marginTop: '10rem' }}>
+          <Container style={{ margin: '10rem 2rem' }}>
 
             {_.map(projectFile, (project, index) => <ProjectsCard
                 project={project} key={index}/>)}
-
-            {/*<Item.Group divided*/}
-            {/*            style={{*/}
-            {/*              backgroundColor: 'rgba(255, 255, 255, 0.10)',*/}
-            {/*              padding: '3rem 3rem',*/}
-            {/*              marginBottom: '1rem',*/}
-            {/*            }}*/}
-            {/*            className={'project'}>*/}
-            {/*  <Item>*/}
-            {/*    <Item.Image*/}
-            {/*        src='https://jhsup.github.io/images/internbit-logo.png'/>*/}
-            {/*    <Item.Content>*/}
-            {/*      <Item.Header as='a'>InternBit</Item.Header>*/}
-            {/*      <Item.Meta>*/}
-            {/*        <span style={{ color: 'white' }}>*/}
-            {/*          2020-07-28*/}
-            {/*        </span>*/}
-            {/*      </Item.Meta>*/}
-            {/*      <Item.Description style={{ color: 'white' }}>*/}
-            {/*        InternBit is a web application that seeks to make internship searching easier.*/}
-            {/*      </Item.Description>*/}
-            {/*      <Item.Extra>*/}
-            {/*        <Label style={{backgroundColor: '#00000059', color: 'white'}}>*/}
-            {/*          React.js*/}
-            {/*        </Label>*/}
-            {/*        <Label>Web Scraping</Label>*/}
-            {/*        <Label>Summer Research Project</Label>*/}
-            {/*        <Button  floated='right' style={{backgroundColor: '#3c4044', color: 'white'}}>*/}
-            {/*          Read More*/}
-            {/*          <Icon name='right chevron'/>*/}
-            {/*        </Button>*/}
-            {/*      </Item.Extra>*/}
-            {/*    </Item.Content>*/}
-            {/*  </Item>*/}
-            {/*</Item.Group>*/}
 
           </Container>
         </div>
