b5c6c4e95c5c124eed2e56b59296901a3bf96f50
Jenny <jhsu2@hawaii.edu>
2020-10-03 01:07:46 -1000

    BLESS

--- app/src/App.js
+++ app/src/App.js
@@ -3,15 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './ui/pages/HomePage';
 import About from './ui/pages/About';
-import { BrowserRouter as Router, Route, } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import TopMenu from './ui/components/TopMenu';
 
 function App() {
   return (
       <Router>
         <TopMenu/>
-        <Route path='/' component={HomePage}/>
-        <Route path='/about' component={About}/>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route exact path='/about' component={About}/>
+        </Switch>
       </Router>
   );
 }
