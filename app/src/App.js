4e7069d95b0d5acbfedbbdfcdf07763eab90d470
Jenny <jhsu2@hawaii.edu>
2020-10-04 02:32:46 -1000

    images work for project

--- app/src/App.js
+++ app/src/App.js
@@ -5,6 +5,7 @@ import Projects from './ui/pages/Projects';
 import Project from './ui/components/Project';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import TopMenu from './ui/components/TopMenu';
+import AboutMe from './ui/pages/AboutMe';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <TopMenu/>
         <Switch>
           <Route exact path='/' component={HomePage}/>
+          <Route exact path='/about' component={AboutMe}/>
           <Route exact path='/projects' component={Projects}/>
           <Route exact path='/projects/:name' component={Project}/>
         </Switch>
